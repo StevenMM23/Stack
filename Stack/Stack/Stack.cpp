4e4ff57b71fda4fa49f899df58cfe1bf10163118
Steven Martinez <esteven123123@outlook.com>
2021-11-30 17:12:16 -0400

    Add comments

--- Stack/Stack/Stack.cpp
+++ Stack/Stack/Stack.cpp
@@ -8,11 +8,14 @@
 #include <iostream>
 using namespace std;
 
+//Funcion para pausar y limpiar la consola
 void WaitClear()
 {
 	system("pause");
 	system("cls");
 }
+
+// Clase nodo que contiene las propiedades data y Nodo tipo puntero llamado siguiente
 class Node
 {
 	public:
@@ -20,99 +23,145 @@ class Node
 		Node* next;
 };
 
-
+// Clase Stack que contiene un nodo puntero llamado top y las funciones que tiene Pila
 class Stack
 {
 	private:
 		Node* top;
 	public:
-		Stack() { top = NULL; }
+		//Constructor que define el estado inicial de la propiedad top
+		Stack() { top = NULL; } 
 		void push(int x);
 		int pop();
 		void Display();
 };
 
+//Metedo Push de la Clase Stack
 void Stack::push(int x)
 {
+	// Se crea un nodo puntero llamado t
 	Node *t = new Node;
+
+	// Programacion Defensiva
 	if (t == NULL)
 		cout << "Stack is Full";
+	// Sino...
 	else
 	{
+		// Al nodo *t se le asigna a data el valor de x (Parametro)
 		t->data = x;
+
+		// El puntero next de t apunta al nodo anterior que tiene top
 		t->next = top;
+
+		// top para a ser t que es el nuevo nodo creado y su next apunta al nodo anterior
 		top = t;
 	}
 }
 
 int Stack::pop()
 {
+	// -1 es el valor por defecto del Stack
 	int x = -1;
+
+	// Si top esta apuntando a NULL (Constructor) significa que la pila esta vacia
 	if (top == NULL)
-		cout << "La pila esta vacia\n";
+		cout << "The Stack is Empty\n";
+
+	// De otra forma...
 	else
 	{
+		// se le asigna a x el valor que tiene ese nodo que se eliminara
 		x = top->data;
+
+		// t sera el nodo que tiene el top
 		Node* t = top;
+
+		// Top se va para el nodo anterior (No se quiere involucrar en el lio de eliminacion)
 		top = top->next;
+
+		// Se elimina t pero Top logro escapar al nodo que apuntaba t (El anterior)
 		delete t;
 	}
+	// Retorna x...
 	return x;
 }
 
 void Stack::Display()
 {
+	// Se declara un nodo *p que lo igualamos a top
 	Node* p = top;
+
+	// Mientras ese nodo no este apuntando a NULL....
 	while(p != NULL)
 	{
+		// Imprime el dato que tiene guardado p
 		cout << p->data << " ";
+
+		// Mueve al nodo que esta apuntando P y asi sucesivamente 
 		p = p->next;
 	}
+	// Espacio...
 	cout << endl;
 }
 
 int main()
 {
+	//Declaro la clase Stack para utilizar sus metodos
 	Stack stk;
+
+	// Declaro las variables que se utilizaran para el menu
 	int answer,value,y;
+
 	do 
 	{
-		cout << "Elija que desea hacer\n";
-		cout << "1. Push()" << endl;
-		cout << "2. Pop()" << endl;
-		cout << "3. Display()" << endl;
+		cout << "Choose a option: \n";
+		cout << "1. Push" << endl;
+		cout << "2. Pop" << endl;
+		cout << "3. Display" << endl;
 		cout << "4. Exit" << endl;
-		cout << "\nRespuesta: ";
+		cout << "\nAnswer: ";
+
 		cin >> answer;
 
+		// La respuesta introducida se evalua para saber en cual case cae
 		switch (answer)
 		{
 		case 1: system("cls");
-			cout << "Escribe el numero que desea ingresar: ";
+			cout << "Input a number: ";
 			cin >> value;
+
+			// Introduce el valor que desea en la pila 
 			stk.push(value);
-			cout << "El numero " << value << " se ha ingresado correctamente!\n";
+			cout << "The number" << value << " was inserted\n";
 			WaitClear();
 			break;
 
 		case 2: system("cls");
+
+			// Guarda en la variable y el elemento que fue eliminado de la pila (El ultimo elemento)
 			y = stk.pop();
-			if(y != -1)
-				cout << "Se ha eliminado el numero " << y << " de la pila\n";
+
+			// Si retorna diferente a -1 significa que elimino un numero y dira cual numero fue
+			if(y != -1) 
+				cout << "The number " << y << " has been deleted\n";
 			WaitClear();
 			break;
 
 		case 3: system("cls");
-			cout << "Los numeros que estan en la pila son: \n";
+			cout << "The numbers on the stack are: \n";
+
+			// Muestras los numeros de la pila
 			stk.Display();
 			WaitClear();
 			break;
 
-		case 4:
-			cout << "El programa ha finalizado !\n";
+		case 4: //Finaliza el programa
+
+			cout << "The program has ended !\n";
 			break;
-		default: system("cls");
-			cout << "No es una opcion....\n";
+		default: system("cls"); // En caso de que el numero no este en las opciones
+			cout << "It's not an option....\n";
 			WaitClear();
 			break;
 		}
